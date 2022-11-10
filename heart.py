41b24821db3ab9ea7a3039993e501040cc057b25
nattapongchantong <n0637230548@gmail.com>
2022-11-10 20:54:31 +0700

    first commit

---
+++ heart.py
@@ -0,0 +1,33 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor('white')
+t.hideturtle()
+t.goto(0,-10)
+
+t.pensize(3)
+t.color('red')
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.setheading(60)
+t.circle(-90,200)
+t.forward(178)
+t.end_fill()
+
+t.penup()
+t.goto(-150,130)
+t.pendown()
+t.color('white')
+t.write("มหาลัยเชียงใหม่",font=("Verdana",20,"bold"))
+
+t.penup()
+t.goto(-240,-100)
+t.pendown()
+t.color('black')
+t.write("Nattapong Chnagtong",font=("Verdana",30,"bold"))
+
+turtle.done()
